0d30031434861f33061901ec5f1e260e63c46394
Ishan Goel <ishangoel04062001@gmail.com>
2021-11-09 12:18:59 +0530

    Delete sem5/COA lab/Computer-Organization-and-Architecture-master/lab directory

--- sem5/COA lab/Computer-Organization-and-Architecture-master/lab/Assgn1_Grp_14/Half_Adder/HA.v
+++
@@ -1,6 +0,0 @@
-module half_adder(s,c,a,b);		//	Defining Module
-    output s,c;					//	Declaring s and c as outputs
-    input a,b;					//	Declaring a and b as inputs 
-    xor g1(s,a,b);				//	s = a xor b
-    and g2(c,a,b);				//	c = a and b
-endmodule						//	Ending Module
--- sem5/COA lab/Computer-Organization-and-Architecture-master/lab/KGP_RISC/KGP_RISC/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/ishan/Desktop/sem5/COA lab/KGP_RISC/KGP_RISC/Instruction_decoder_Tb_isim_beh.exe" -prj "/home/ishan/Desktop/sem5/COA lab/KGP_RISC/KGP_RISC/Instruction_decoder_Tb_beh.prj" "work.Instruction_decoder_Tb" "work.glbl" 
